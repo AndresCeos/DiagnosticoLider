b106fabc0603d14c840b60bf3be4d95e4b9f57f7
DOC-weed <aleesfeo1234@gmail.com>
2025-03-21 14:43:57 -0700

    wip

--- src/components/Redactores.js
+++ src/components/Redactores.js
@@ -1,4 +1,4 @@
-import { list } from '@vercel/blob';
+import { head, list } from '@vercel/blob';
 import { useState, useEffect } from 'react';
 
 
@@ -10,36 +10,50 @@ export default function Redactores() {
         listBriefFiles();
     }, []);
   
+    const fetchFiles = async () => {
+        try {
+          const response = await fetch('/api/lists-files');
+          if (!response.ok) throw new Error('Error al listar archivos');
+          const data = await response.json();
+          setFiles(data);
+          console.log(data)
+        } catch (error) {
+          console.log();
+        }
+      };
    const listBriefFiles = async () => {
+        // Aquí se construye la llamada a la API de Vercel Blob
+        const  b  = await fetch('/api/lists-files', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            mode: 'no-cors'
+        });
     
         // Listar archivos con prefijo "brief/"
-        const { blobs } = await list({
-        token: "vercel_blob_rw_KwdI4XyihBuH5ui9_aAvPjetIZhwukC2fUsDQO0zsf8zRVd", // Token con permisos de lectura
-        limit: 1000, // Máximo de archivos a listar
-        headers: { 'Access-Control-Allow-Origin': '*',  }
-        }).then(res => {
-            console.log(res.json())
-        }).catch(err => console.error(err));
+         /*const b = await list({
+            token: 'vercel_blob_rw_KwdI4XyihBuH5ui9_aAvPjetIZhwukC2fUsDQO0zsf8zRVd',
+            limit: 1000 // Token con permisos de lectura
+            }, {headers: {'Access-Control-Allow-Origin': '*'}, mode: 'no-cors'});*/
 
-        setFiles(blobs.map(blob => ({
+            setFiles(b);
+        /*setFiles(blobs.map(blob => ({
             name: blob.pathname.replace('brief/', ''), // Remover prefijo de carpeta
             url: blob.url,
             size: blob.size,
             uploadedAt: blob.uploadedAt
             })));
-        console.log(files);
+        console.log(files);*/
     };
+    console.log(files)
+    
     return(
         <div>
             <h1>Documentos</h1>
             {files.length === 0 && <p>No hay archivos</p>}
-            {files.map(file => (
-                <div key={file.name}>
-                <a href={file.url} target="_blank" rel="noopener noreferrer">
-                    {file.name}
-                </a>
-                </div>
-            ))}
+            
         </div>
     )
 }
